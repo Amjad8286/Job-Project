567c0bac7ac4b2b7aa50c28ffcebbd2161c67896
ansari.amjad8286@gmail.com <ansari.amjad8286@gamil.com>
2021-07-23 20:16:01 +0530

    changed by amjad new branch

--- about.php
+++ about.php
@@ -36,37 +36,7 @@
               </div>
             </div>
           </div>
-          <div class="carousel-item">
-            <div class="container ">
-              <div class="row">
-                <div class="col-md-5">
-                  <div class="detail-box">
-                    <h1>
-                      For All Your <br>
-                      best salary Needs
-                    </h1>
-                    <p>
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quidem maiores perspiciatis, illo
-                      maxime voluptatem a itaque suscipit.
-                    </p>
-                    <div class="btn-box">
-                      <a href="" class="btn1">
-                        Contact Us
-                      </a>
-                      <a href="" class="btn2">
-                        About Us
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-7">
-                  <div class="img-box">
-                    <img src="https://images.unsplash.com/photo-1586281380349-632531db7ed4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" width=550px height=350px pxalt="">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          
           <div class="carousel-item">
             <div class="container ">
               <div class="row">
